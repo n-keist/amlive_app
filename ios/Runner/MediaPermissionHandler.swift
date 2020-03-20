54b41fcf317cbae400fb7f3f8a9104b80c2a6cd6
Nikolaj Keist <nikolaj@Nikolajs-MacBook-Pro.local>
2020-03-20 21:02:07 +0100

    more on publishing

---
+++ ios/Runner/MediaPermissionHandler.swift
@@ -0,0 +1,8 @@
+//
+//  MediaPermissionHandler.swift
+//  Runner
+//
+//  Created by Nikolaj Keist on 20.03.20.
+//
+
+import Foundation
---
+++ ios/Runner/NativeViewFactory.swift
@@ -0,0 +1,8 @@
+//
+//  NativeViewFactory.swift
+//  Runner
+//
+//  Created by Nikolaj Keist on 03.03.20.
+//
+
+import Foundation
