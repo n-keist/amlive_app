5fb52e4674b2220b48bed61face8d8daa0a6e3fd
Nikolaj K <keistnikolaj@gmail.com>
2020-07-04 15:23:45 +0200

    app storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:amlive/app.dart';
 
-void main() => runApp(AmliveApp());
+void main() {
+  (() async {
+    await Hive.initFlutter();
+    await Hive.openBox('app');
+    await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp],
+    );
+  })();
+  runApp(AmliveApp());
+}
